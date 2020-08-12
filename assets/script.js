5c4771f9e73602ac50e6bfe0ec02b285dc38f9f7
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-08-12 16:03:02 +0200

    array country added

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,10 @@ canvas.id = "chart1";
 let ctx = document.getElementById("chart1").getContext("2d");
 let x = document.getElementById("tbodyChart1");
 let list = document.getElementById("table1").getElementsByTagName("th");
+let listTd = document.getElementById("table1").getElementsByTagName("td");
+
 let arrayTest = [];
+let arrayCountry=[];
 
 //var arrayLignes = document.getElementById("table1").cells;
 
@@ -28,13 +31,23 @@ for (let i = 0; i < list.length; i++) {
         arrayTest.push(list[i].innerHTML);
     }
 }
+for( let j=0;j<listTd.length;j++){
+    if(listTd[j].innerHTML.search(/[A-Z]/g)>=0) {
+        arrayCountry.push(listTd[j].innerHTML);
+    }
+}
+console.log(arrayCountry);
 function datasets(label, borderColor, data) {
     this.label = label;
     this.borderColor = borderColor;
     this.data = data;
   }
   let dataTest = [105,125,135,145];
-  let z = new datasets("Test","green",dataTest);
+  let z = new datasets(arrayCountry[3],"green",dataTest);
+
+  
+
+
   console.log(z);
 
 console.log(arrayTest);
@@ -47,7 +60,7 @@ let chart1 = new Chart(ctx, {
         datasets: [
             {
                
-             label: "Belgium",
+             label: arrayCountry[0],
              borderColor: "rgb(255, 99, 132)",
                  data: [
                     75,
@@ -65,4 +78,5 @@ let chart1 = new Chart(ctx, {
         ],
     },
     options: {},
+
 });
